12631377004fc2b622e84b498f9865c0e6179344
givebest <givenlovs@msn.com>
2023-04-18 15:12:35 +0800

    feat:Fix Glarity not showing, remove page summary

--- src/content-script/index.tsx
+++ src/content-script/index.tsx
@@ -1,10 +1,7 @@
 import { render } from 'preact'
 import '@/assets/styles/base.scss'
-import { getUserConfig } from '@/config'
 import ChatGPTTip from '@/content-script/compenents/ChatGPTTip'
-import { config } from '@/content-script/search-engine-configs'
 import Browser from 'webextension-polyfill'
-import PageSummary from '@/content-script/compenents/PageSummary'
 import mount from '@/content-script/compenents/Mount'
 import getQuestion from './compenents/GetQuestion'
 import { siteConfig as sietConfigFn } from './utils'
@@ -12,27 +9,6 @@ import '@/content-script/styles.scss'
 
 const siteConfig = sietConfigFn()
 async function Run() {
-  const userConfig = await getUserConfig()
-  const siteRegex = new RegExp(
-    Object.values(config)
-      .map((v) => {
-        return v.regex
-      })
-      .join('|'),
-  )
-  const container = document.createElement('section')
-  container.className = 'glarity--summary'
-  document.body.prepend(container)
-  render(
-    <PageSummary
-      pageSummaryEnable={userConfig.pageSummaryEnable}
-      pageSummaryWhitelist={userConfig.pageSummaryWhitelist}
-      pageSummaryBlacklist={userConfig.pageSummaryBlacklist}
-      siteRegex={siteRegex}
-    />,
-    container,
-  )
-
   const questionData = await getQuestion()
   if (questionData) {
     mount(questionData)
